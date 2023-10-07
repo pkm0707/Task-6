9e89f3589cab70e6c7de54ca88d6477867f55f73
Praveen kumar Muthiah <145551426+pkm0707@users.noreply.github.com>
2023-10-07 15:34:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Write a “person” class to hold all the details.
+// Question (3)
 class person 
 {
     constructor(name,dob,email,qualification,gender,maritalStatus,phoneno,address) 
@@ -18,7 +18,7 @@ let output_2 = new person("Sakthi Kumar M", "08.04.1999","sakthi.guvi@gmail.com"
 console.log(output_1)
 console.log(output_2)
 //---------------------------------------------------------------------------------------------------------------------------------------------
-// write a class to calculate the uber price (1)
+// Question (4)
 class uber 
 {
     constructor(date,start,end,vehicletype,km,price)
@@ -45,3 +45,93 @@ console.log(uberprice_4)
 console.log(uberprice_5)
 console.log(uberprice_6)
 //---------------------------------------------------------------------------------------------------------------------------------------------
+// Question (1)
+// (a),(d)
+class movie
+{
+    constructor(t,s,r)
+    {
+        this.title  = t
+        this.studio = s
+        this.rating = r
+    }
+}
+let ad_result_1 = new movie("title of the movie","studio that made the movie","PG13")
+let ad_result_2 = new movie("Casino Royale","Eon Productions","PG13")
+console.log(ad_result_1)
+console.log(ad_result_2)
+// (b)
+class Movie
+{
+    constructor(t,s)
+    {
+        this.title  = t
+        this.studio = s
+        this.rating = "PG"
+    }
+}
+let b_result_1 = new Movie("title of the movie","studio that made the movie")
+let b_result_2 = new Movie("Casino Royale","Eon Productions")
+console.log(b_result_1)
+console.log(b_result_2)
+// ---------------------------------------------------------------------------------------------------------------------------------------
+// Question (2)
+class circle
+{
+    constructor(radius,color)
+    {
+        this.radius = 1.0
+        this.color  = "Red"
+        if (typeof (radius) !== "undefined")
+        {
+            this.radius = radius;
+        }
+        if (typeof (color) !== "undefined")
+        {
+            this.color = color;
+        }
+    }
+    getRadius() 
+    {
+        return this.radius
+    }
+    setRadius(radius) 
+    {
+        this.radius = radius
+    }
+    getColor() 
+    {
+        return this.color
+    }
+    setColor(color) 
+    {
+        this.color = color
+    }
+    toString() 
+    {
+        return "Radius: " + this.radius + " Color: " + this.color
+    }
+    getArea() 
+    {
+        return (2 * (Math.PI) * this.radius)
+    }
+    getCircumference() 
+    {
+        return (2 * this.radius)
+    }
+}
+let c1 = new circle();
+console.log("Constructor with no params: " + c1.toString());
+let c2 = new circle(3.5);
+console.log("Constructor with one param: " + c2.toString());
+let c3 = new circle(2.2, 'Yellow');
+console.log("getRadius: " + c3.getRadius());
+c3.setRadius(3.3);
+console.log("Radius value after setRadius: " + c3.getRadius());
+console.log("getColor: " + c3.getColor());
+c3.setColor("Blue");
+console.log("Color Value after setColor: " + c3.getColor());
+console.log(c3.toString());
+console.log("Area: " + c3.getArea());
+console.log("Circumference: " + c3.getCircumference());
+// ---------------------------------------------------------------------------------------------------------------------------------------
